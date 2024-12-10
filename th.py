3504f4765018f3c4ba89973ea14f6ce300f15192
nayanarora <aroranayan66@gmail.com>
2024-12-11 09:54:48 +1100

    3

--- th.py
+++ th.py
@@ -1,2 +1,16 @@
 print("nayan")
 print("arora")
+using System;
+
+public class MathUtils
+{
+    public static double Average(int a, int b)
+    {
+        return a + b / 2;
+    }
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Average(2, 1));
+    }
+}
